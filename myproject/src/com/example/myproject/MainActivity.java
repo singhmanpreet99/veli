87a0d1eb5ac95b1bf69e816c529876ba87999a15
CURSE <singhmanpreet99@yahoo.com>
2012-09-18 22:59:40 +0530

    te

---
+++ myproject/src/com/example/myproject/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myproject;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //hello comment hai yar
+        //yooyoyoyoy
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
